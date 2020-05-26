27f3e5cb33f47943bc08b69ab8d3c65ed700bdbd
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-26 13:54:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ('Mr. Chocolate', 1, 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Rowdy', 2, 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Tabitha', 6, 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown', 4, 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Melissa', 13, 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Grinch', 6, 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Wendy', 6, 'female');
+INSERT INTO bears (name) VALUES (null);

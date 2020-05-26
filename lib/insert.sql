fabb4bfd47cfc9732c621ae9802908d1cc0beba1
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-26 13:47:44 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 24, "M", "dark brown", "calm", 0),
+("Rowdy", 8, "F", "black", "angery", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "green", "Slimy", 0),
+("Melissa", 15, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "dark black", "spoopy", 1),
+("Wendy", 6, "F", "blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);

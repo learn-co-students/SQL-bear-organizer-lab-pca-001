45f5b40e9e66e6d194486b4621783204eecfd8ba
Liam Tinney <ltinney@powerhrg.com>
2020-05-26 14:32:42 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 34, "male", "brown", "peaceful", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 15, "male", "white", "restless", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 24, "female", "black", "reserved", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 38, "male", "brown", "impulsive", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 16, "female", "brown", "passive", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 23, "male", "green", "agressive", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 15, "female", "red", "easy-going", "yes");
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive) VALUES (NULL, 23, "male", "white", "agressive", "no");
